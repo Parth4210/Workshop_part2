e2bc5dc76b05844fa4d5496e9f13ebc2aed9cdea
Alpha4210 <parth712007@gmail.com>
2025-09-25 12:17:22 +0530

    Added increase/decrease button

--- app/src/main/java/com/parth/workshop_part2/ui/theme/AnimatedBtn2.kt
+++ app/src/main/java/com/parth/workshop_part2/ui/theme/AnimatedBtn2.kt
@@ -6,18 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +30,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,7 +41,8 @@ class AnimatedBtn2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val counter = remember{ mutableStateOf(0) }
+            val counter = remember { mutableStateOf(0) }
+            val oldCounter = remember { mutableStateOf(0) }
             Workshop_part2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(
@@ -43,20 +50,53 @@ class AnimatedBtn2 : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(innerPadding),
+                            .padding(innerPadding)
                     ) {
-                        Button(
-                            onClick = {
-                                counter.value += 1
+                        Row {
+                            OutlinedButton(
+                                shape = RoundedCornerShape(
+                                    10.dp,
+                                    0.dp,
+                                    0.dp,
+                                    10.dp,
+                                ),
+                                onClick = {
+                                    oldCounter.value = counter.value-1
+                                    counter.value += 1
+                                }) {
+                                Text("INCREASE", fontWeight = FontWeight.Bold)
                             }
-                        ) {
-                            Text("Add to counter")
+                            OutlinedButton(
+                                shape = RoundedCornerShape(
+                                    0.dp,
+                                    10.dp,
+                                    10.dp,
+                                    0.dp,
+                                ),
+                                onClick = {
+                                    oldCounter.value = counter.value-1
+                                    counter.value -= 1
+                                }) {
+                                Text("REDUCE", fontWeight = FontWeight.Bold)
+                            }
                         }
+
                         AnimatedContent(
                             targetState = counter.value,
+//                            transitionSpec = { fadeIn() + scaleIn() + slideInHorizontally() togetherWith fadeOut() + scaleOut() + slideOutHorizontally() },
                             transitionSpec = {
-                                slideInVertically() togetherWith slideOutVertically { targetOffset ->
-                                    targetOffset
+                                if (counter.value > oldCounter.value) {
+                                    slideInVertically { targetOffset ->
+                                        +targetOffset
+                                    } togetherWith slideOutVertically { targetOffset ->
+                                        -targetOffset
+                                    }
+                                } else {
+                                    slideInVertically { targetOffset ->
+                                        -targetOffset
+                                    } togetherWith slideOutVertically { targetOffset ->
+                                        +targetOffset
+                                    }
                                 }
                             }
                         ) { content ->
